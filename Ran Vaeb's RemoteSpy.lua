3f0ed45cae05b20d37fa508b5028f61564499822
Abodiey <97450871+Abodiey@users.noreply.github.com>
2023-10-25 21:57:03 +0300

    Include Usage message

--- Ran Vaeb's RemoteSpy.lua
+++ Ran Vaeb's RemoteSpy.lua
@@ -1,6 +1,6 @@
 -- ORIGINAL SOURCE: loadstring(game:HttpGet("https://pastebin.com/raw/yJzq5ceV"))()
 -- TO USE THIS:
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/"))()
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Abodiey/Roblox/main/Ran%20Vaeb's%20RemoteSpy.lua"))()
 
 --[[
 
