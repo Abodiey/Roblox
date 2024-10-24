e556248206ccd5bda292e331148c7d8e98c661e3
Abodiey <97450871+Abodiey@users.noreply.github.com>
2024-10-24 21:06:29 +0300

    Update AbodieysFruitHop.lua

--- scripts/AbodieysFruitHop.lua
+++ scripts/AbodieysFruitHop.lua
@@ -103,7 +103,7 @@ if not friendIngame and anyFruitFound then
 	game:GetService("Players").LocalPlayer.CharacterAdded:Connect(function(char)
 		char.ChildAdded:Connect(onCharacterChild)
 	end)
-	local TPSpeed = 200
+	local TPSpeed = 350
 	for i,v in ipairs(game:GetService("Workspace"):GetChildren()) do
 		if v and v:FindFirstChild("Fruit") and v:FindFirstChild("Handle") then
 			print("> Found "..v.Name)
@@ -142,6 +142,45 @@ if not friendIngame and anyFruitFound then
 	end
 	game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("TeleportToSpawn")
 end
+local function HopServer()
+	local function Hop()
+		for i = 1, 200 do
+			local serverlist = game:GetService("ReplicatedStorage").__ServerBrowser:InvokeServer(i)
+			for k,v in pairs(serverlist) do
+				if k ~= game.JobId and v.Count < 9 then
+					game:GetService("ReplicatedStorage").__ServerBrowser:InvokeServer("teleport",k)
+					return true
+				end
+			end
+		end
+		return false
+	end
+	if not getgenv().Loaded then
+		local function child(childinstance)
+			if childinstance.Name == "ErrorPrompt" then
+				if childinstance.Visible then
+					if childinstance.TitleFrame.ErrorTitle.Text == "Teleport Failed" then
+						HopServer()
+					end
+				end
+				childinstance:GetPropertyChangedSignal("Visible"):Connect(function()
+					if childinstance.Visible then
+						if childinstance.TitleFrame.ErrorTitle.Text == "Teleport Failed" then
+							HopServer()
+						end
+					end
+				end)
+			end
+		end
+		for k,v in pairs(game:GetService("CoreGui").RobloxPromptGui.promptOverlay:GetChildren()) do
+			child(v)
+		end
+		game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(child)
+		getgenv().Loaded = true
+	end
+	while not Hop() do task.wait() end
+end
+
 local countdownTime = 5
 local retryTime = 60
 local betweenClickTime = 1
@@ -186,49 +225,12 @@ repeat task.wait()
 		rejoinAccepted = true
 	end
 until rejoinAccepted--first sea: 2753915549, second sea: 4442272183, third sea: 7449423635
-local function HopServer()
-	local function Hop()
-		for i = 1, 200 do
-			local serverlist = game:GetService("ReplicatedStorage").__ServerBrowser:InvokeServer(i)
-			for k,v in pairs(serverlist) do
-				if k ~= game.JobId and v.Count < 9 then
-					game:GetService("ReplicatedStorage").__ServerBrowser:InvokeServer("teleport",k)
-					return true
-				end
-			end
-		end
-		return false
-	end
-	if not getgenv().Loaded then
-		local function child(childinstance)
-			if childinstance.Name == "ErrorPrompt" then
-				if childinstance.Visible then
-					if childinstance.TitleFrame.ErrorTitle.Text == "Teleport Failed" then
-						HopServer()
-					end
-				end
-				childinstance:GetPropertyChangedSignal("Visible"):Connect(function()
-					if childinstance.Visible then
-						if childinstance.TitleFrame.ErrorTitle.Text == "Teleport Failed" then
-							HopServer()
-						end
-					end
-				end)
-			end
-		end
-		for k,v in pairs(game:GetService("CoreGui").RobloxPromptGui.promptOverlay:GetChildren()) do
-			child(v)
-		end
-		game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(child)
-		getgenv().Loaded = true
-	end
-	while not Hop() do task.wait() end
-end
-repeat
-	local pingSuccess = (game:HttpGet("example.com") and true) or false
-	repeat task.wait() until pingSuccess ~= nil
-	if pingSuccess then
-		HopServer()
-		task.wait()
-	end
-until not game:GetService("Players").LocalPlayer
+hopServer()
+-- repeat
+-- 	local pingSuccess = (game:HttpGet("example.com") and true) or false
+-- 	repeat task.wait() until pingSuccess ~= nil
+-- 	if pingSuccess then
+-- 		HopServer()
+-- 		task.wait()
+-- 	end
+-- until not game:GetService("Players").LocalPlayer
