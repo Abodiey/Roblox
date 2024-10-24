dad57a54a4063f2517e703dc7658613e8278ec11
Abodiey <97450871+Abodiey@users.noreply.github.com>
2024-10-24 21:27:36 +0300

    Update AbodieysFruitHop.lua

--- scripts/AbodieysFruitHop.lua
+++ scripts/AbodieysFruitHop.lua
@@ -1,3 +1,4 @@
+print("Abodiey's FH")
 if not game:IsLoaded() then game.Loaded:Wait() end
 if game.PlaceId ~= 2753915549 and game.PlaceId ~= 4442272183 and game.PlaceId ~= 7449423635 then return end
 --gui start
