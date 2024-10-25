57213449c3058b91b8f8f72e7b2fc537eef070f5
Abodiey <97450871+Abodiey@users.noreply.github.com>
2024-10-25 10:03:59 +0300

    Update Fruit MeshID Saver.lua

--- scripts/Fruit MeshID Saver.lua
+++ scripts/Fruit MeshID Saver.lua
@@ -1,15 +1,20 @@
-wait(5)
 if not game:IsLoaded() then
 	game.Loaded:Wait()
 end
+wait(5)
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
-	local filename = "blox_fruits.txt"
+	local filename = "blox_fruits_meshids.txt"
 	pcall(function()
 		if not isfile(filename) then
 			repeat pcall(function() writefile(filename,"~") end) task.wait() until isfile(filename)
 		end
 	end)
 	local function get_file_content()
+		pcall(function()
+			if not isfile(filename) then
+				repeat pcall(function() writefile(filename,"~") end) task.wait() until isfile(filename)
+			end
+		end)
 		local file_content
 		repeat pcall(function() file_content = readfile(filename) end) task.wait() until file_content
 		return file_content
@@ -17,7 +22,7 @@ if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7
 	local function saveFruitData(id,fruitName)
 		local file_content = get_file_content()
 		repeat task.wait() until file_content
-		if id and id ~= "" and fruitName and fruitName ~= "" and fruitName ~= "Fruit" and fruitName ~= "Fruit " and fruitName ~= " Fruit" then
+		if id and id ~= "" and fruitName and fruitName ~= "" and fruitName ~= "Fruit" and fruitName ~= "Fruit " and fruitName ~= " Fruit" and fruitName ~= "(Eating)" then
 			if not file_content:find(fruitName) then
 				local text = id.." = "..fruitName
 				if file_content:find("~") then
