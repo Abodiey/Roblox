da8bcef25f0bbab7029ce2f01abb975367fe7ee3
Abodiey <97450871+Abodiey@users.noreply.github.com>
2024-11-16 09:49:04 +0300

    Update AutoReconnect.lua

--- scripts/AutoReconnect.lua
+++ scripts/AutoReconnect.lua
@@ -1,32 +1,39 @@
 if not game:IsLoaded() then
     game.Loaded:Wait()
 end
-local currentPlaceId = game.PlaceId
 local currentJobId = game.JobId
 game:GetService("GuiService").ErrorMessageChanged:Connect(function()
-	local success,okbutton = pcall(function()
-		return game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ErrorPrompt.MessageArea.ErrorFrame.ButtonArea.OkButton.ButtonText
+	local LeaveButtonExists, _ = pcall(function()
+		return game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ErrorPrompt.MessageArea.ErrorFrame.ButtonArea.LeaveButton.ButtonText
 	end)
-	if okbutton then game:GetService("GuiService"):ClearError() return end
-	if #game.Players:GetPlayers() <= 1 then
-		game.Players.LocalPlayer:Kick("\nRejoining...")
-		task.wait(1)
-		repeat
-			local pingSuccess = (game:HttpGet("example.com") and true) or false
-			repeat task.wait() until pingSuccess ~= nil
-			if pingSuccess then
-				game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
-			end
-			task.wait(10)
-		until currentJobId ~= game.JobId
-	else
-		repeat
-			local pingSuccess = (game:HttpGet("example.com") and true) or false
-			repeat task.wait() until pingSuccess ~= nil
-			if pingSuccess then
-				game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
-			end
-			task.wait(10)
-		until currentJobId ~= game.JobId
+	local ReconnectButtonExists, _ = pcall(function()
+		return game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ErrorPrompt.MessageArea.ErrorFrame.ButtonArea.ReconnectButton.ButtonText
+	end)
+	if ReconnectButtonExists or LeaveButtonExists then
+		if #game:GetService("Players"):GetPlayers() <= 1 then
+			game.Players.LocalPlayer:Kick("\nRejoining...")
+			task.wait(1)
+			repeat
+				local pingSuccess = (game:HttpGet("example.com") and true) or false
+				if pingSuccess == nil then
+					repeat task.wait() until pingSuccess ~= nil
+				end
+				if pingSuccess == true then
+					game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+				end
+				task.wait(10)
+			until currentJobId ~= game.JobId
+		else
+			repeat
+				local pingSuccess = (game:HttpGet("example.com") and true) or false
+				if pingSuccess == nil then
+					repeat task.wait() until pingSuccess ~= nil
+				end
+				if pingSuccess == true then
+					game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
+				end
+				task.wait(10)
+			until currentJobId ~= game.JobId
+		end
 	end
 end)
