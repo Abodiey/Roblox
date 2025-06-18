022b277b1081338d90d98badbce43ce72ecc38ea
Abodiey <97450871+Abodiey@users.noreply.github.com>
2025-06-18 05:04:56 +0300

    Update growagarden_autobuy.lua

--- scripts/growagarden_autobuy.lua
+++ scripts/growagarden_autobuy.lua
@@ -14,7 +14,13 @@ _G.Configuration = {
 	["Auto-Buy-Seeds"] = true,
 	["Auto-Buy-Gear"] = true,
 	["Auto-Buy-Eggs"] = true,
-	["Auto-Buy-Bee-Egg"] = true
+	["Auto-Buy-Bee-Egg"] = true,
+	["Auto-Craft-Anti-Bee-Egg"] = true,
+	["Auto-Craft-Crafters-Seed-Pack"] = true,
+	--// Options
+	["Seeds-To-Buy"] = {"Carrot","Strawberry","Blueberry","Orange Tulip","Tomato","Corn","Daffodil","Pumpkin","Apple","Bamboo","Coconut","Cactus","Dragon Fruit","Mango","Grape","Mushroom","Pepper","Cacao","Beanstalk","Ember Lily","Sugar Apple"},
+	["Gears-To-Buy"] = {"Watering Can","Trowel","Recall Wrench","Basic Sprinkler","Advanced Sprinkler","Godly Sprinkler","Lightning Rod","Master Sprinkler"},
+	["Eggs-To-Buy"] = {1,2,3}
 }
 
 
@@ -68,13 +74,12 @@ GuiService.ErrorMessageChanged:Connect(function()
 	TeleportService:TeleportToPlaceInstance(PlaceId, JobId, LocalPlayer)
 end)
 
-
-_G.Seeds = {"Carrot","Strawberry","Blueberry","Orange Tulip","Tomato","Corn","Daffodil","Pumpkin","Apple","Bamboo","Coconut","Cactus","Dragon Fruit","Mango","Grape","Mushroom","Pepper","Cacao","Beanstalk","Ember Lily","Sugar Apple"}
 task.spawn(function()
 while GetConfigValue("Enabled") do
 	local AutoBuySeeds = GetConfigValue("Auto-Buy-Seeds")
 	if AutoBuySeeds then
-		for _,Seed in pairs(_G.Seeds) do
+		local SeedsToBuy = GetConfigValue("Seeds-To-Buy")
+		for _,Seed in pairs(SeedsToBuy) do
 			game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuySeedStock"):FireServer(Seed)
 			task.wait()
 		end
@@ -84,13 +89,14 @@ end
 end)
 
 --_G.Gears = {"Watering Can","Trowel","Recall Wrench","Basic Sprinkler","Advanced Sprinkler","Godly Sprinkler","Lightning Rod","Master Sprinkler","Cleaning Spray","Favorite Tool","Harvest Tool","Friendship Pot"}
-_G.Gears = {"Watering Can","Trowel","Recall Wrench","Basic Sprinkler","Advanced Sprinkler","Godly Sprinkler","Lightning Rod","Master Sprinkler"}
 task.spawn(function()
 while GetConfigValue("Enabled") do
 	local AutoBuyGear = GetConfigValue("Auto-Buy-Gear")
 	if AutoBuyGear then
-		for _,Gear in pairs(_G.Gears) do
+		local GearsToBuy = GetConfigValue("Gears-To-Buy")
+		for _,Gear in pairs(GearsToBuy) do
 			game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuyGearStock"):FireServer(Gear)
+			task.wait()
 			task.wait()
 		end
 	end
@@ -98,12 +104,12 @@ while GetConfigValue("Enabled") do
 end
 end)
 
-_G.Eggs = {1,2,3}
 task.spawn(function()
 while GetConfigValue("Enabled") do
 	local AutoBuyEggs = GetConfigValue("Auto-Buy-Eggs")
 	if AutoBuyEggs then
-		for _,Egg in pairs(_G.Eggs) do
+		local EggsToBuy = GetConfigValue("Eggs-To-Buy")
+		for _,Egg in pairs(EggsToBuy) do
 			game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuyPetEgg"):FireServer(Egg)
 			task.wait(1)
 		end
@@ -117,6 +123,115 @@ while GetConfigValue("Enabled") do
 	local AutoBuyBeeEgg = GetConfigValue("Auto-Buy-Bee-Egg")
 	if AutoBuyBeeEgg then
 		game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("BuyEventShopStock"):FireServer("Bee Egg")
+	end
+	task.wait(60)
+end
+end)
+
+task.spawn(function()
+while GetConfigValue("Enabled") do
+	local AutoCraftAntiBeeEgg = GetConfigValue("Auto-Craft-Anti-Bee-Egg")
+	if AutoCraftAntiBeeEgg then
+				local args = {
+			"Claim",
+			workspace:WaitForChild("Interaction"):WaitForChild("UpdateItems"):WaitForChild("NewCrafting"):WaitForChild("EventCraftingWorkBench"),
+			"GearEventWorkbench",
+			1
+		}
+		game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("CraftingGlobalObjectService"):FireServer(unpack(args))
+		local BeeEgg
+			for _,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+				if v and v.Name and string.match(v.Name, "^".."Bee Egg") then
+					BeeEgg = v
+				break
+			end
+		end
+		if not BeeEgg then
+			for _,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+				if v and v.Name and string.match(v.Name, "^".."Bee Egg") then
+					BeeEgg = v
+					break
+				end
+			end
+		end
+		if not BeeEgg then task.wait(10) return end
+		game.Players.LocalPlayer.Character.Humanoid:EquipTool(BeeEgg)
+		local args = {
+			"Cancel",
+			workspace:WaitForChild("Interaction"):WaitForChild("UpdateItems"):WaitForChild("NewCrafting"):WaitForChild("EventCraftingWorkBench"),
+			"GearEventWorkbench"
+		}
+		game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("CraftingGlobalObjectService"):FireServer(unpack(args))
+		task.wait(1)
+		local args = {
+			"SetRecipe",
+			workspace:WaitForChild("Interaction"):WaitForChild("UpdateItems"):WaitForChild("NewCrafting"):WaitForChild("EventCraftingWorkBench"),
+			"GearEventWorkbench",
+			"Anti Bee Egg"
+		}
+		game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("CraftingGlobalObjectService"):FireServer(unpack(args))
+		task.wait(1)
+		local prompt = Workspace.Interaction.UpdateItems.NewCrafting.EventCraftingWorkBench:FindFirstChild("CraftingProximityPrompt", true)
+		fireproximityprompt(prompt)
+		task.wait(1)
+		local args = {
+			"Craft",
+			workspace:WaitForChild("Interaction"):WaitForChild("UpdateItems"):WaitForChild("NewCrafting"):WaitForChild("EventCraftingWorkBench"),
+			"GearEventWorkbench"
+		}
+		game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("CraftingGlobalObjectService"):FireServer(unpack(args))
+	end
+	task.wait(30)
+	local AutoCraftCraftersSeedPack = GetConfigValue("Auto-Craft-Crafters-Seed-Pack")
+	if AutoCraftCraftersSeedPack then
+		local args = {
+			"Claim",
+			workspace:WaitForChild("Interaction"):WaitForChild("UpdateItems"):WaitForChild("NewCrafting"):WaitForChild("SeedEventCraftingWorkBench"),
+			"SeedEventWorkbench",
+			1
+		}
+		game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("CraftingGlobalObjectService"):FireServer(unpack(args))
+		local FlowerSeedPack 
+		for _,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+			if v and v.Name and string.match(v.Name, "^".."Flower Seed Pack") then
+				FlowerSeedPack = v
+				break
+			end
+		end
+		if not FlowerSeedPack then
+			for _,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+				if v and v.Name and string.match(v.Name, "^".."Flower Seed Pack") then
+					FlowerSeedPack = v
+					break
+				end
+			end
+		end
+		if not FlowerSeedPack then task.wait(10) return end
+		game.Players.LocalPlayer.Character.Humanoid:EquipTool(FlowerSeedPack)
+		local args = {
+			"Cancel",
+			workspace:WaitForChild("Interaction"):WaitForChild("UpdateItems"):WaitForChild("NewCrafting"):WaitForChild("SeedEventCraftingWorkBench"),
+			"SeedEventWorkbench"
+		}
+		game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("CraftingGlobalObjectService"):FireServer(unpack(args))
+		task.wait(1)
+		local args = {
+			"SetRecipe",
+			workspace:WaitForChild("Interaction"):WaitForChild("UpdateItems"):WaitForChild("NewCrafting"):WaitForChild("SeedEventCraftingWorkBench"),
+			"SeedEventWorkbench",
+			"Crafters Seed Pack"
+		}
+		game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("CraftingGlobalObjectService"):FireServer(unpack(args))
+		task.wait(1)
+		local prompt = Workspace.Interaction.UpdateItems.NewCrafting.SeedEventCraftingWorkBench:FindFirstChild("CraftingProximityPrompt, true")
+		fireproximityprompt(prompt)
+		task.wait(1)
+		local args = {
+			"Craft",
+			workspace:WaitForChild("Interaction"):WaitForChild("UpdateItems"):WaitForChild("NewCrafting"):WaitForChild("SeedEventCraftingWorkBench"),
+			"SeedEventWorkbench"
+		}
+		game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("CraftingGlobalObjectService"):FireServer(unpack(args))
 	end
 	task.wait(60)
 end
