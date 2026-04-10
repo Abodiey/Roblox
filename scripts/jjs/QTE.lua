78f081f07fb1435e7d15c0077e00747c2bbd506b
Abodiey <97450871+Abodiey@users.noreply.github.com>
2026-04-10 10:49:01 +0300

    Update and rename auto_qte.lua to QTE.lua

--- scripts/jjs/QTE.lua
+++ scripts/jjs/QTE.lua
@@ -1,27 +1,26 @@
--- File: auto_qte.lua
+local QTE = {}
 local VIM = game:GetService("VirtualInputManager")
-local LocalPlayer = game:GetService("Players").LocalPlayer
-local qteSettings = { checkMS = 10, randomness = 150 }
 
-return function(getQTEActive)
-    local connection = LocalPlayer.PlayerGui.ChildAdded:Connect(function(child)
-        if getQTEActive() and child.Name == "QTE" then
+function QTE.Init(State)
+    local conn = game:GetService("Players").LocalPlayer.PlayerGui.ChildAdded:Connect(function(child)
+        if State.Toggles.QTE and child.Name == "QTE" then
             local label = child:WaitForChild("QTE_PC", 5)
             if label then
                 task.spawn(function()
-                    while getQTEActive() and label.Parent == child do
+                    while State.Toggles.QTE and label.Parent == child do
                         local key = label.Text:match("%a")
                         if key then
-                            task.wait(math.random(0, qteSettings.randomness) / 1000)
                             VIM:SendKeyEvent(true, Enum.KeyCode[key], false, game)
                             task.wait(0.1)
                             VIM:SendKeyEvent(false, Enum.KeyCode[key], false, game)
                         end
-                        task.wait(qteSettings.checkMS / 1000)
+                        task.wait(0.01)
                     end
                 end)
             end
         end
     end)
-    return connection
+    table.insert(State.Connections, conn)
 end
+
+return QTE
