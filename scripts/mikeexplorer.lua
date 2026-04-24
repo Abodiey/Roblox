c8c643897d6e2959b792c180098fae9a70c5f4b2
Abodiey <97450871+Abodiey@users.noreply.github.com>
2026-04-24 20:54:18 +0300

    Update mikeexplorer.lua

--- scripts/mikeexplorer.lua
+++ scripts/mikeexplorer.lua
@@ -1,3 +1,4 @@
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/Abodiey/Roblox/refs/heads/main/scripts/mikeexplorer.lua"))()
 if not game:IsLoaded() then game.Loaded:Wait() end
 
 if safe_mode == nil then 
