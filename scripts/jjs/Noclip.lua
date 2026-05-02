3f5b0bb102db74eb40ba4c227269edbbb691f2f5
Abodiey <97450871+Abodiey@users.noreply.github.com>
2026-05-02 16:30:36 +0300

    Update Noclip.lua

--- scripts/jjs/Noclip.lua
+++ scripts/jjs/Noclip.lua
@@ -6,11 +6,8 @@ function Noclip.Init(State)
     local lp = Players.LocalPlayer
 
     task.spawn(function()
-        while true do
-            task.wait(0.1) -- Runs 10 times a second (No lag, still effective)
-            
-            if not State.Toggles.NoclipPlayers then continue end
-
+        task.wait(0.1)
+        while State.Toggles.Noclip do
             local myChar = lp.Character
             for _, char in pairs(charFolder:GetChildren()) do
                 if char == myChar then continue end
@@ -22,10 +19,9 @@ function Noclip.Init(State)
                     end
                 end
             end
+            task.wait(0.1)
         end
     end)
-
-    return Noclip
 end
 
 return Noclip
