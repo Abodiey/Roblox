b718a15ea8983a1975b4b06a9a3e632d066ef717
Abodiey <97450871+Abodiey@users.noreply.github.com>
2026-05-12 02:11:07 +0300

    Update DomainNoclip.lua

--- scripts/jjs/DomainNoclip.lua
+++ scripts/jjs/DomainNoclip.lua
@@ -1,20 +1,17 @@
+local DomainNoclip = {}
+
 local domains = workspace:WaitForChild("Domains")
 local RunService = cloneref(game:GetService("RunService"))
-
-local DomainNoclip = {}
 
 function DomainNoclip.Init(State)
     local connection = RunService.Stepped:Connect(function()
         local enabled = State.Toggles.DomainNoclip
-        if not enabled then return end
-            
         for _, v in pairs(domains:GetChildren()) do
             if v:IsA("BasePart") then 
                 v.CanCollide = not enabled 
             end
         end
     end)
-
     table.insert(State.Connections, connection)
 end
 
