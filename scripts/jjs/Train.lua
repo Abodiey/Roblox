9dff0aa4ca1d050fad5de4e618e83654bb68e8fc
Abodiey <97450871+Abodiey@users.noreply.github.com>
2026-05-22 18:16:56 +0300

    Update Train.lua

--- scripts/jjs/Train.lua
+++ scripts/jjs/Train.lua
@@ -4,7 +4,7 @@ function Train.Init()
     local Map = workspace:FindFirstChild("Map")
     if Map then
         local Remote = Map:FindFirstChild("Train", true)
-        if Remote then
+        if Remote and Remote:IsA("RemoteEvent") then
             Remote:FireServer()
         end
     end
