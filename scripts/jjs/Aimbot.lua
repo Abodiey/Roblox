4e87caceb2dcbc42025fb3275ba6f6bff1c36988
Abodiey <97450871+Abodiey@users.noreply.github.com>
2026-06-10 11:27:20 +0300

    Update Aimbot.lua

--- scripts/jjs/Aimbot.lua
+++ scripts/jjs/Aimbot.lua
@@ -7,10 +7,14 @@ local UserInputService = cloneref(game:GetService("UserInputService"))
 local GuiService = cloneref(game:GetService("GuiService"))
 
 local Player = Players.LocalPlayer
-local workspace = cloneref(game:GetService("Workspace"))
 local Camera = workspace.CurrentCamera
 
+while not Player or not Player.Parent or not Camera or not Camera.Parent do
+    task.wait()
+end
+
 local Characters = workspace:WaitForChild("Characters")
+
 
 while CoreGui:FindFirstChild("AimbotHighlight") do 
     CoreGui.AimbotHighlight:Destroy() 
