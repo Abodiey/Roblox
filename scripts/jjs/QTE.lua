1e94a3edb22d275e91e01dda0a3be15f85be615d
Abodiey <97450871+Abodiey@users.noreply.github.com>
2026-06-10 19:02:08 +0300

    Update QTE.lua

--- scripts/jjs/QTE.lua
+++ scripts/jjs/QTE.lua
@@ -2,25 +2,32 @@ local QTE = {}
 local VIM = cloneref(game:GetService("VirtualInputManager"))
 local Players = cloneref(game:GetService("Players"))
 local Player = Players.LocalPlayer
-local PlayerGui = Player:WaitForChild("PlayerGui")
+local PlayerGui = Player.PlayerGui
 
 function QTE.Init(State)
     local conn = PlayerGui.ChildAdded:Connect(function(child)
         if State.Toggles.QTE and child.Name == "QTE" then
-            local label = child:WaitForChild("QTE_PC", 5)
-            if label then
-                task.spawn(function()
-                    while State.Toggles.QTE and label.Parent == child do
-                        local key = label.Text:match("%a")
-                        if key then
-                            VIM:SendKeyEvent(true, Enum.KeyCode[key], false, game)
-                            task.wait() task.wait()
-                            VIM:SendKeyEvent(false, Enum.KeyCode[key], false, game)
-                        end
-                        task.wait(1/10)
+            local label = child:FindFirstChild("QTE_PC")
+            if not label then return end
+
+            task.spawn(function()
+                while State.Toggles.QTE and child.Parent == PlayerGui do
+                    local healthBar = child.Health.Bar1
+
+                    if healthBar.Size.X.Scale > 0.75 then
+                        task.wait(0.1)
+                        continue
                     end
-                end)
-            end
+
+                    local key = label.Text:match("%a")
+                    if key then
+                        local keyCode = Enum.KeyCode[key:upper()]
+                        VIM:SendKeyEvent(true, keyCode, false, game)
+                        task.wait()
+                        VIM:SendKeyEvent(false, keyCode, false, game)
+                    end
+                end
+            end)
         end
     end)
     table.insert(State.Connections, conn)
