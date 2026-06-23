fb05b1b953b8e32f7358091d51595cb7ca1c54f7
Abodiey <97450871+Abodiey@users.noreply.github.com>
2026-06-23 12:17:08 +0300

    Update Noclip.lua

--- scripts/jjs/Noclip.lua
+++ scripts/jjs/Noclip.lua
@@ -69,14 +69,14 @@ function Noclip.Init(State)
                         end
                     elseif v.Name == "SetAssets" then
                         -- Highly specific path optimization for GojoMask
-                        local mask = v:FindFirstChild("GojoMask") and v.GojoMask:FindFirstChild("Mask")
+                        local mask = v:GetAttribute("Moveset") == "Gojo" and v:FindFirstChild("GojoMask") and v.GojoMask:FindFirstChild("Mask")
                         if mask and mask:IsA("BasePart") and mask.CanCollide then
                             storedParts[mask] = true
                             v.GojoMask.Mask.CanCollide = false
                         end
 
                         -- Highly specific path optimization for ArmWrap
-                        local armWrap = v:FindFirstChild("ArmWrap") and v.ArmWrap:FindFirstChild("Collide")
+                        local armWrap = v:GetAttribute("Moveset") == "Hanami" and v:FindFirstChild("ArmWrap") and v.ArmWrap:FindFirstChild("Collide")
                         if armWrap and armWrap:IsA("BasePart") and armWrap.CanCollide then
                             storedParts[armWrap] = true
                             v.ArmWrap.CanCollide = false
