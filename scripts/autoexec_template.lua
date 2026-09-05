93ad3db38a6246217cea86ef1e34c1dd8c8c8f02
Abodiey <97450871+Abodiey@users.noreply.github.com>
2026-09-05 13:39:50 +0300

    Delete scripts/autoexec_template.lua

--- scripts/autoexec_template.lua
+++
@@ -1,8 +0,0 @@
-local URL = "https://raw.githubusercontent.com/Abodiey/Roblox/main/scripts/NAMEHERE"
-repeat
-	task.wait()
-	pcall(function()
-		_ = game:HttpGet(URL)
-	end)
-until _
-loadstring(_)()
